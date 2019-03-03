97c4064d34ccf42f9f85f7b8d4f3c785430453b4
erikringwalters <erik.ring.walters@gmail.com>
2019-03-02 21:46:28 -0800

    deployment

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-// server.js
+//Install express server
+const express = require('express');
 const path = require('path');
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/ngxcp'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/ngxcp/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
