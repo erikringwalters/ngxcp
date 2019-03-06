92540623532b5286a2500384a7d4cae1c6196d36
erikringwalters <erik.ring.walters@gmail.com>
2019-03-05 23:10:12 -0800

    bubble chart working

--- src/app/bubble-chart1/bubble-chart1.component.ts
+++ src/app/bubble-chart1/bubble-chart1.component.ts
@@ -10,70 +10,99 @@ export class BubbleChart1Component implements OnInit {
  
   multi: any[] = [
     {
-      name: 'Green',
-      series: [
+      "name": "Germany",
+      "series": [
         {
-          name: 5,
-          value: 2650
+          "name": "2010",
+          "x": 40632,
+          "y": 80.3,
+          "r": 80.4
         },
         {
-          name: 10,
-          value: 2800      
+          "name": "2000",
+          "x": 36953,
+          "y": 80.3,
+          "r": 78
         },
         {
-          name: 15,
-          value: 2000
-        },
-        {
-          name: 20,
-          value: 2800      
-        }, 
-        {
-          name: 25,
-          value: 9100      
-        }, 
-        {
-          name: 30,
-          value: 1300      
-        },
-        {
-          name: 35,
-          value: 150      
-        },
+          "name": "1990",
+          "x": 31476,
+          "y": 75.4,
+          "r": 79
+        }
       ]
     },
     {
-      name: 'Red',
-      series: [
+      "name": "USA",
+      "series": [
         {
-          name: 5,
-          value: 2500
+          "name": "2010",
+          "x": 49737,
+          "y": 78.8,
+          "r": 310
         },
         {
-          name: 10,
-          value: 3100
+          "name": "2000",
+          "x": 45986,
+          "y": 76.9,
+          "r": 283
         },
         {
-          name: 15,
-          value: 2350
+          "name": "1990",
+          "x": 3706,
+          "y": 75.4,
+          "r": 253
+        }
+      ]
+    },
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "2010",
+          "x": 36745,
+          "y": 81.4,
+          "r": 63
         },
         {
-          name: 20,
-          value: 100
-        }, {
-          name: 25,
-          value: 1500
-        }, {
-          name: 30,
-          value: 10
-        }, {
-          name: 35,
-          value: 10000
+          "name": "2000",
+          "x": 34774,
+          "y": 79.1,
+          "r": 59.4
+        },
+        {
+          "name": "1990",
+          "x": 29476,
+          "y": 77.2,
+          "r": 56.9
+        }
+      ]
+    },
+    {
+      "name": "United Kingdom",
+      "series": [
+        {
+          "name": "2010",
+          "x": 36240,
+          "y": 80.2,
+          "r": 62.7
+        },
+        {
+          "name": "2000",
+          "x": 32543,
+          "y": 77.8,
+          "r": 58.9
+        },
+        {
+          "name": "1990",
+          "x": 26424,
+          "y": 75.7,
+          "r": 57.1
         }
       ]
     }
   ];
-
+  
   view: any[] = [700, 400];
 
   // options
