58382a7c6d9f9f27ceedbd9ca70afff31d18235c
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-04-15 20:43:48 -0700

    working on a scatterplot

--- src/app/plotly2/plotly2.component.ts
+++ src/app/plotly2/plotly2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import data from '../../data/np_rec_out_company.txt';
+import data from '../../data/np_rec_out_company.js';
 
 
 @Component({
@@ -9,14 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Plotly2Component implements OnInit {
 
- // multi: any =  data;
+
+  dat: Array<number> = data;
   constructor() { }
 
   ngOnInit() {
   }
   public graph = {
     data: [
-        { x: [1, 2, 3, 4, 5, 6, 7, 9], y: [2, 6, 3, 10, 12, 1, 20, 10], type: 'scatter', mode: 'markers', marker: {color: 'black', background: 'blue'} },
+        { x: [1, 2, 3, 4, 5, 6, 7, 9], y: this.dat, type: 'scatter', mode: 'markers', marker: {color: 'black', background: 'blue'} },
        // { x: [1, 2, 3, 4, 5, 6, 7, 8], y: [2, 5, 3, 4, 5, 6, 7, 8], type: 'bar' },
     ],
     layout: {width: 1000, height: 500, title: 'A Fancy Plot'}
